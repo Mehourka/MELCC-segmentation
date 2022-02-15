623b2d7263d7d383b51f0eca3067b081717c08be
Karim Mehour <karim.mehour@cgq.qc.ca>
2022-02-15 11:23:17 -0500

    First commit

--- Test-Segmentation.py
+++ Test-Segmentation.py
@@ -1,10 +1,13 @@
-import functions 
+
 import gdal
 import ogr
 import os
 from skimage import exposure
 from skimage.segmentation import quickshift, felzenszwalb
 import time
+import numpy as np
+import gc
+from skimage.util import img_as_ubyte, img_as_uint
 
 
 def resize_raster(fn, dest):
@@ -20,27 +23,38 @@ def resize_raster(fn, dest):
     out_fn = imgDir + fn.split(sep = "/")[-1][:-4] + f"_rescaled.tif"
     gdal.Warp(out_fn, ds, height = y, width = x, resampleAlg="cubic")
     
+    ds.FlushCache()
+    ds = None
+    
     return out_fn
 
 def segmentation(img_fn, dest, scale = 100, sigma = 1, min_size=50):    
     if not os.path.isdir(dest) : os.mkdir(dest)
     
     ref_ds, bdata_ortho = functions.readFiles(img_fn)
-    toc = time.time() #timer
-    print("segments start ")
-    img = exposure.rescale_intensity(bdata_ortho)
     
+    bdata_ortho = bdata_ortho.astype(np.uint8)
+    print(bdata_ortho.dtype)
+    
+ 
+    toc = time.time() #timer
+    img = exposure.rescale_intensity(bdata_ortho, out_range=np.uint8)
+    img = img_as_ubyte(bdata_ortho)
+    print("segments start :  ")
     # Segmentation Felzenszwalb
-    segments = felzenszwalb(img, scale=scale, sigma=sigma, min_size=min_size)
+    segments =img_as_uint(felzenszwalb(img, scale=scale, sigma=sigma, min_size=min_size)) 
     
     #Segmentation QuickShift
-    
-    segments_fn = os.path.join(dest, "segments_{}x{}x{}.tif".format(int(scale),int(sigma),int(min_size)))
+    # segments = quickshift(img, convert2lab=False)
+    segments_fn = os.path.join(dest, os.path.basename(img_fn).replace("rescaled", "segments"))
     functions.writeRaster(segments, segments_fn, ref_ds)
     polygonize_raster(segments_fn, )
     tic= time.time()
 
-    print(f"{segments_fn} crée en : {tic-toc}")
+    print(f"Segmentation done. \n {segments_fn} crée en : {tic-toc} seconds ")
+    
+    ref_ds.FlushCache()
+    ref_ds = bdata_ortho = None
     
     return segments_fn
 
@@ -56,7 +70,7 @@ def polygonize_raster(raster_fn, out_fn=""):
     
     #Creation du shapeFile
     driver = ogr.GetDriverByName("ESRI Shapefile")
-    out_ds = driver.CreateDataSource(raster_fn.replace(".tif", "_blank.shp") )
+    out_ds = driver.CreateDataSource(raster_fn.replace(".tif", ".shp") )
     out_lyr = out_ds.CreateLayer("Segments", srs = ras_srs)
     
     #Ajout des champs d'attributs
@@ -68,11 +82,13 @@ def polygonize_raster(raster_fn, out_fn=""):
     # Fonction de vectorisation
     gdal.Polygonize(band, None, out_lyr, 0, [])
     
+    out_ds.FlushCache()
+    raster_ds.FlushCache()
     out_ds = raster_ds = out_lyr = None
-
  
 # #input
 os.chdir(os.path.dirname(__file__))
+import functions 
 imgDir = '../02-inputs/'
 
 #Output
@@ -80,19 +96,20 @@ resultats= '../03-resultats/'
 if not os.path.isdir(resultats) :   os.mkdir(resultats) #créer le dossier si inexistant
 
 #Scan les fichier dans le dossier d'inputs
-for folder in [_.path for _ in os.scandir(imgDir) if _.is_dir()][0:1]:
-    ortho_fn = [_.path for _ in os.scandir(folder) if _.is_file() if not "rescaled" in _.path][0]
-    
-    ortho_rescaled = resize_raster(ortho_fn, imgDir)
-    
-    seg_dir = os.path.join(folder, "segments")
-    if not os.path.exists(seg_dir) :
-        os.mkdir(seg_dir)
+for folder in [_.path for _ in os.scandir(imgDir) if _.is_dir()]:
+    files = [_.path for _ in os.scandir(folder) if not "rescaled" in _.path if ".tif" in _.path]
+    for ortho_fn in files:
+        print(ortho_fn)
         
-    for i in [100,200]:
-        for j in [1,2]:
-            for k in [50,150]:
-                segmentation(ortho_rescaled, seg_dir, i,j,k)
+        ortho_rescaled = resize_raster(ortho_fn, imgDir)
+        print("Rescaling Done.")
+        seg_dir = os.path.join(folder, "segments")
+        if not os.path.exists(seg_dir) :
+            os.mkdir(seg_dir)
+        
+        gc.collect()
+        segmentation(ortho_rescaled, seg_dir, 150, 0.8, 100)
+        
     
     
     
